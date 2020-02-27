820461816072c8afe0087bddf11e2f663af9c99f
keith <xqk559@gmail.com>
2020-02-27 08:13:01 -0500

    Added action creators for orde form submission async axios POST

---
+++ src/Store/actions/order.js
@@ -0,0 +1,30 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        typse: actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData,
+    };
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAILED,
+        error: error,
+    };
+};
+
+export const purchaseBurgerStart = (orderData) => {
+    return dispatch => {
+        axios.post( 'https://burgerbuilder-cea69.firebaseio.com/orders.json', orderData)
+            .then( response => {
+                console.log(response.data);
+                dispatch(purchaseBurgerSuccess(response.data, orderData))
+            } )
+            .catch( error => {
+                dispatch(purchaseBurgerFail(error));
+            } );
+    };
+};
