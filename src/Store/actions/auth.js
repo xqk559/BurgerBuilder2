300f086800149f49f1f78e5ae2377ba7850c19e0
keith <xqk559@gmail.com>
2020-02-28 11:33:41 -0500

    Built a reducer for auth

--- src/Store/actions/auth.js
+++ src/Store/actions/auth.js
@@ -7,10 +7,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData,
+        idToken: token,
+        uderId: userId,
     };
 };
 
@@ -36,7 +37,7 @@ export const auth = (email, password, signup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
-                dispatch(authSuccess(response.data))
+                dispatch(authSuccess(response.data.idToken, response.data.localId))
             })
     };
 };
--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import reducer from './Store/reducers/burgerBuilder';
 import thunk from 'redux-thunk';
 import orderReducer from './Store/reducers/order';
+import authReducer from './Store/reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: reducer,
     order: orderReducer,
+    auth: authReducer,
 });
 
 const store = createStore(rootReducer, composeEnhancers(
