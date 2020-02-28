ec1cb5d975a915c035d2d0c23b9df4785f33fb64
keith <xqk559@gmail.com>
2020-02-28 13:43:10 -0500

    Logout button now dispatches logout action creator and redirects to home as unauthenticated user

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Checkout from './Containers/Checkout/Checkout';
 import {Route} from 'react-router-dom';
 import Orders from './Containers/Orders/Orders';
 import Auth from './Containers/Auth/auth';
+import Logout from './Containers/Auth/logout/logout';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
        <Route path="/checkout" component={Checkout} />
        <Route path="/auth" component={Auth} />
        <Route path="/orders" component={Orders} />
+       <Route path="/logout" component={Logout} />
      </Layout>
     </div>
   );
--- src/Store/actions/index.js
+++ src/Store/actions/index.js
@@ -12,4 +12,5 @@ export {
 
 export {
         auth,
+        logout,
 } from './auth';
