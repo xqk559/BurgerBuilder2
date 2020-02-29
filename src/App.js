11bfcb136b7048eb4072fcd529b14538972c53fc
keith <xqk559@gmail.com>
2020-02-29 11:09:57 -0500

    Fixed CSS issue with burger centering

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Layout from './Components/Layouts/Layout' ;
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder' ;
 import Checkout from './Containers/Checkout/Checkout';
-import {Route, withRouter} from 'react-router-dom';
+import {Route, withRouter, Switch, Redirect} from 'react-router-dom';
 import Orders from './Containers/Orders/Orders';
 import Auth from './Containers/Auth/auth';
 import Logout from './Containers/Auth/logout/logout';
@@ -14,20 +14,42 @@ class App extends Component {
     this.props.onTryAutoSignup();
   }
   render () {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if ( this.props.isAuthenticated ) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
     return (
       <div>
-      <Layout>
-      <Route path="/" exact component={BurgerBuilder} />
-        <Route path="/home" component={BurgerBuilder} />
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/auth" component={Auth} />
-        <Route path="/orders" component={Orders} />
-        <Route path="/logout" component={Logout} />
-      </Layout>
+        <Layout>
+          {routes}
+        </Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -35,4 +57,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
