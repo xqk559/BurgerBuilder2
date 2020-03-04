1d21bcacf1bada30ee169e4316d6461585ca05c8
keith <xqk559@gmail.com>
2020-03-04 13:30:48 -0500

    Added unit test suite for BurgerBuilder Container by only importing the BurgerBuilder class

--- src/Components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/Components/Navigation/NavigationItems/NavigationItems.test.js
@@ -21,7 +21,7 @@ describe('<NavigationItems />', ()=>{
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
-    it('should render three <NavigationItem /> elements if authenticated', ()=>{
+    it('should show logout button if authenticated', ()=>{
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true);
     });
--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -10,7 +10,7 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import { connect } from 'react-redux';
 import * as burgerBuilderActions from '../../Store/actions/index';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
     //     this.state = {...}
